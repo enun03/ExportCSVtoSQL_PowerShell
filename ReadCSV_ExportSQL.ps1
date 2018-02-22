384ea7a64d8730a48ef410209d33f27201f6d21c
xcs4766 <nunezle@utcaus.com>
2018-02-22 15:12:11 -0400

    first commit

---
+++ ReadCSV_ExportSQL.ps1
@@ -0,0 +1,39 @@
+﻿#Importing CSV 
+$csv = Import-Csv test.csv
+$items = @()
+
+#SQL DB Config
+$dbconn = New-Object System.Data.SqlClient.SqlConnection
+$dbconn.ConnectionString = "Server='servername'; Database='databasename'; Integrated Security=TRUE"
+$cmd = $dbconn.CreateCommand()
+
+#Building Query
+$query = "INSERT INTO table (Field1, Field2, Field3) VALUES (@name, @last, @age)"
+$cmd.CommandText = $query
+$cmd.Parameters.Add("@name", $Name) | Out-Null
+$cmd.Parameters.Add("@last", $Last) | Out-Null
+$cmd.Parameters.Add("@age", $Age) | Out-Null
+
+#Reading CSV 
+$csv | ForEach-Object {
+    #Allocating CSV items into custom object
+    $items += New-Object -Type PSCustomObject -Property `
+    @{
+        'Name' = $_.Name
+        'Last' = $_.Last
+        'Age'  = $_.Age
+    }
+}
+
+#Exporting CSV data to SQL DB
+$dbconn.Open()
+
+foreach($item in $items) {
+    $Name = $item.Name
+    $Last = $item.Last
+    $Age = $item.Age
+    #Exec Query
+    $cmd.ExecuteNonQuery()
+}
+
+$dbconn.Close()
